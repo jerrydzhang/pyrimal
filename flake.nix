3fd6ceace3b26f2dba23772b76e3993f108c35c4
jerrydzhang <jerry.zhang.d@gmail.com>
2026-03-03 12:03:05 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,54 +1,14 @@
-# {
-#   description = "Python Development Enviornment";
-#
-#   inputs = {
-#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#   };
-#
-#   outputs = {nixpkgs, ...}: let
-#     inherit (nixpkgs) lib;
-#     forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-#   in {
-#     devShells = forAllSystems (
-#       system: let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#       in {
-#         default =
-#           (pkgs.buildFHSEnv {
-#             name = "python-dev";
-#
-#             targetPkgs = p:
-#               with p; [
-#                 stdenv.cc.cc
-#                 glibc
-#                 zlib
-#
-#                 python3
-#                 uv
-#               ];
-#
-#             profile = ''
-#               unset PYTHONPATH
-#               uv sync --inexact
-#               uv pip install -e . -e "jernerics @ ./../jernerics"
-#               . .venv/bin/activate
-#             '';
-#
-#             runScript = "zsh";
-#           }).env;
-#       }
-#     );
-#   };
-# }
 {
   description = "Develop Python on Nix with uv";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixgl.url = "github:guibou/nixGL";
+
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, nixgl, ... }:
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
@@ -57,46 +17,38 @@
       devShells = forAllSystems (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
+            overlays = [ nixgl.overlay ];
+          };
         in
+
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              (python3.withPackages (
-                ps: with ps; [
-                  ruff
-                  pytest
-                  mypy
-                ]
-              ))
-              pyright
               uv
+              nixgl.default
             ];
-
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-              pkgs.stdenv.cc.cc.lib
-              pkgs.zlib
-            ];
-            # LD_LIBRARY_PATH = [
-            #   "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib"
-            # ];
 
             shellHook = ''
               unset PYTHONPATH
-              uv sync --inexact
+              uv sync
               . .venv/bin/activate
             '';
           };
 
           hpc = pkgs.mkShell {
             packages = with pkgs; [
-              python3
               uv
+              nixgl.default
             ];
 
             shellHook = ''
               unset PYTHONPATH
-              uv sync --inexact
+              uv sync
               . .venv/bin/activate
             '';
           };
